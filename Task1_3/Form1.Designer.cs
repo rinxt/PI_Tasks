022f1b54b240a653e45329f9b760ddd82ddcdcf1
Arina <arinevercc@gmail.com>
2024-03-30 04:37:44 +0300

    added Task1_3

---
+++ Task1_3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Task1_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Размерность матрицы:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(167, 16);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(338, 16);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 2;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(273, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "строк";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(444, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 15);
+            label3.TabIndex = 4;
+            label3.Text = "столбцов";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(532, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Сгенерировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(167, 52);
+            label4.Name = "label4";
+            label4.Size = new Size(10, 15);
+            label4.TabIndex = 6;
+            label4.Text = " ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(126, 409);
+            label5.Name = "label5";
+            label5.Size = new Size(377, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Циклический сдвиг элементов каждой строки на 3 элемента влево";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(666, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Task1_3/Form1.cs
@@ -0,0 +1,88 @@
+﻿namespace Task1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool textBox1Valid = int.TryParse(textBox1.Text, out int rows) && rows >= 2 && rows <= 10;
+            bool textBox2Valid = int.TryParse(textBox2.Text, out int columns) && columns >= 2 && columns <= 10;
+
+            if (textBox1Valid && textBox2Valid)
+            {
+
+                for (int i = Controls.Count - 1; i >= 0; i--)
+                {
+                    Control control = Controls[i];
+                    if (control is TextBox textBox &&
+                        textBox.Name != "textBox1" &&
+                        textBox.Name != "textBox2")
+                    {
+                        Controls.Remove(textBox);
+                        textBox.Dispose();
+                    }
+                }
+
+                const int textBoxWidth = 25;
+                const int textBoxHeight = 20;
+                const int spacing = 5;
+                Random random = new Random();
+
+                int[,] leftMatrix = new int[rows, columns];
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        int randomValue = random.Next(-99, 100);
+                        leftMatrix[i, j] = randomValue;
+
+                        TextBox textBox = new TextBox();
+                        textBox.Location = new Point(12 + j * (textBoxWidth + spacing), 60 + label4.Height + i * (textBoxHeight + spacing)); // Учитываем отступ от label4
+                        textBox.Size = new Size(textBoxWidth, textBoxHeight);
+                        textBox.Text = randomValue.ToString();
+                        Controls.Add(textBox);
+                    }
+                }
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        int newIndex = (j + 3) % columns; // Правильное вычисление нового индекса после циклического сдвига на 3 элемента влево
+                        TextBox textBox = new TextBox();
+                        textBox.Location = new Point(32 + columns * (textBoxWidth + spacing) + spacing + j * (textBoxWidth + spacing), 60 + label4.Height + i * (textBoxHeight + spacing)); // Учитываем отступ от label4 и располагаем справа от созданной матрицы
+                        textBox.Size = new Size(textBoxWidth, textBoxHeight);
+                        textBox.Text = leftMatrix[i, newIndex].ToString();
+                        Controls.Add(textBox);
+                    }
+                }
+
+                label4.Text = "";
+            }
+            else
+            {
+                label4.Text = "Введите числа от 2 до 10 в оба поля";
+            }
+        }
+    }
+}
