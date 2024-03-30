874ffe4ca0e2a72c55d42fecfca932ea07a54c4e
Arina <arinevercc@gmail.com>
2024-03-30 05:37:02 +0300

    Task 1_5 added

---
+++ Task1_5/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Task1_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(157, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 40);
+            button1.TabIndex = 1;
+            button1.Text = "Выбор файла";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(443, 15);
+            label2.TabIndex = 2;
+            label2.Text = "В каждой строке удалить все символы, стоящие перед группой из шести цифр";
+            // 
+            // label3
+            // 
+            label3.Location = new Point(0, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 23);
+            label3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 201);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(443, 43);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(476, 281);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+    }
+}
---
+++ Task1_5/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Task1_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Выберите текстовый файл";
+            openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем путь к выбранному файлу
+                string filePath = openFileDialog1.FileName;
+
+                // Читаем все строки из файла
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Создаем регулярное выражение для поиска группы из шести цифр
+                Regex regex = new Regex(@"\d{6}");
+
+                string directoryPath = Path.GetDirectoryName(filePath);
+
+                // Создаем новый файл для записи результатов в той же директории
+                string resultFilePath = Path.Combine(directoryPath, "result_" + Path.GetFileName(filePath));
+                using (StreamWriter writer = new StreamWriter(resultFilePath))
+                {
+                    // Проходим по каждой строке и заменяем все символы перед группой из шести цифр
+                    foreach (string line in lines)
+                    {
+                        // Ищем все совпадения в строке с помощью регулярного выражения
+                        MatchCollection matches = regex.Matches(line);
+
+                        // Если найдены совпадения
+                        if (matches.Count > 0)
+                        {
+                            // Получаем индекс последнего совпадения
+                            int lastIndex = matches[matches.Count - 1].Index;
+
+                            // Записываем в файл часть строки, начиная с последней найденной группы из шести цифр
+                            writer.WriteLine(line.Substring(lastIndex));
+                        }
+                        else
+                        {
+                            // Если совпадений не найдено, записываем строку как есть
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+
+                textBox1.Text = "Готово! Результаты сохранены в файле: " + resultFilePath;
+            }
+
+
+        }
+    }
+}
