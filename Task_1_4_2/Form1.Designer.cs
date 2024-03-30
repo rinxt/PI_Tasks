2701665aa15bb46f9359c58f74767134d9b9d3ee
Arina <arinevercc@gmail.com>
2024-03-30 06:26:39 +0300

    Task 1_4_2 added

---
+++ Task_1_4_2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Task_1_4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(23, 87);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(289, 232);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(334, 87);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(289, 232);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(23, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(289, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Шифровать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(334, 342);
+            button2.Name = "button2";
+            button2.Size = new Size(289, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Дешифровать";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(110, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(437, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Шифртекст";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(222, 15);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 6;
+            label3.Text = "КЛЮЧ";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(270, 12);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(121, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(644, 390);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox3;
+    }
+}
---
+++ Task_1_4_2/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+
+namespace Task_1_4_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string plaintext = textBox1.Text.ToUpper(); 
+            string keyword = textBox3.Text.ToUpper();
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                textBox2.Text = "Введите ключ";
+                return;
+            }
+            string ciphertext = EncryptVigenere(plaintext, keyword);
+            if (ciphertext == "INVALID KEY")
+            {
+                textBox2.Text = "Некорректный ключ";
+            }
+            else
+            {
+                textBox2.Text = ciphertext;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ciphertext = textBox2.Text.ToUpper();
+            string keyword = textBox3.Text.ToUpper();
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                textBox1.Text = "Введите ключ";
+                return;
+            }
+            string decryptedText = DecryptVigenere(ciphertext, keyword);
+            if (decryptedText == "INVALID KEY")
+            {
+                textBox1.Text = "Некорректный ключ";
+            }
+            else
+            {
+                textBox1.Text = decryptedText;
+            }
+        }
+
+        private string EncryptVigenere(string plaintext, string keyword)
+        {
+            foreach (char c in keyword)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return "INVALID KEY";
+                }
+            }
+
+            StringBuilder ciphertext = new StringBuilder();
+
+            int keywordIndex = 0;
+            foreach (char c in plaintext)
+            {
+                if (char.IsLetter(c))
+                {
+                    int shift = keyword[keywordIndex % keyword.Length] - 'A';
+                    char encryptedChar = EncryptChar(c, shift);
+                    ciphertext.Append(encryptedChar);
+                    keywordIndex++;
+                }
+                else
+                {
+                    ciphertext.Append(c);
+                }
+            }
+
+            return ciphertext.ToString();
+        }
+
+        private char EncryptChar(char c, int shift)
+        {
+            return (char)(((c - 'A' + shift) % 26) + 'A');
+        }
+
+        private string DecryptVigenere(string ciphertext, string keyword)
+        {
+            foreach (char c in keyword)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return "INVALID KEY";
+                }
+            }
+
+            StringBuilder plaintext = new StringBuilder();
+
+            int keywordIndex = 0;
+            foreach (char c in ciphertext)
+            {
+                if (char.IsLetter(c))
+                {
+                    int shift = keyword[keywordIndex % keyword.Length] - 'A';
+                    char decryptedChar = DecryptChar(c, shift);
+                    plaintext.Append(decryptedChar);
+                    keywordIndex++;
+                }
+                else
+                {
+                    plaintext.Append(c);
+                }
+            }
+
+            return plaintext.ToString();
+        }
+        private char DecryptChar(char c, int shift)
+        {
+            int decryptedValue = (c - 'A' - shift + 26) % 26;
+            return (char)(decryptedValue + 'A');
+        }
+    }
+}
