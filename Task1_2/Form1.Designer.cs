615133884c3f4a1cf6d94e652818843a69cb2fb0
Arina <arinevercc@gmail.com>
2024-03-30 03:28:13 +0300

    Task 1_2 added

---
+++ Task1_2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Task1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Количество элементов";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(166, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(165, 23);
+            textBox1.TabIndex = 1;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(26, 81);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ScrollBars = ScrollBars.Vertical;
+            textBox2.Size = new Size(141, 185);
+            textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 320);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(366, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Очистить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(366, 81);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 35);
+            button2.TabIndex = 5;
+            button2.Text = "Заполнить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(366, 122);
+            button3.Name = "button3";
+            button3.Size = new Size(152, 49);
+            button3.TabIndex = 6;
+            button3.Text = "Найти среднее арифметическое";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(190, 81);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.Size = new Size(141, 185);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(366, 297);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(152, 38);
+            textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 305);
+            label3.Name = "label3";
+            label3.Size = new Size(305, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Cреднее арифметическое положительных элементов";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(366, 188);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(152, 78);
+            textBox5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(563, 359);
+            Controls.Add(textBox5);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox5;
+    }
+}
---
+++ Task1_2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace Task1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private int[] numbers;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int number) || number < 10 || number > 100)
+            {
+                textBox1.Text = "Введите число от 10 до 100";
+            }
+            else
+            {
+                numbers = GenerateArray(number);
+                string numbersString = string.Join(Environment.NewLine, numbers.Select(n => n.ToString()));
+                textBox2.Text = numbersString;
+
+                int swaps, comparisons;
+                BubbleSort(numbers, out swaps, out comparisons);
+
+                string sortedNumbersString = string.Join(Environment.NewLine, numbers.Select(n => n.ToString()));
+                textBox3.Text = sortedNumbersString;
+
+                textBox5.Text = $"Количество перестановок (M): {swaps}\r\nКоличество сравнений (C): {comparisons}";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (numbers == null)
+            {
+                // Handle the case when numbers array is not initialized
+                textBox4.Text = "Сначала сгенерируйте массив";
+                return;
+            }
+
+            double mean = CalculateArithmeticMeanOfPositiveElements(numbers);
+
+            // Display arithmetic mean in textBox4
+            if (double.IsNaN(mean))
+            {
+                textBox4.Text = "Нет положительных элементов";
+            }
+            else
+            {
+                textBox4.Text = mean.ToString();
+            }
+
+        }
+
+        private int[] GenerateArray(int size)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(1, 101);
+            }
+            return array;
+        }
+
+        private void BubbleSort(int[] array, out int swaps, out int comparisons)
+        {
+            swaps = 0;
+            comparisons = 0;
+            bool swapped;
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    comparisons++;
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap array[j] and array[j+1]
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                        swaps++;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+        }
+
+        private double CalculateArithmeticMeanOfPositiveElements(int[] array)
+        {
+            double sum = 0;
+            int count = 0;
+            foreach (int num in array)
+            {
+                if (num > 0)
+                {
+                    sum += num;
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
+                return sum / count;
+            }
+            else
+            {
+                return double.NaN;
+            }
+        }
+
+    }
+}
+
