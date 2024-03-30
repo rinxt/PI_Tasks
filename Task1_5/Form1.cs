6fce15a55b7908227cde1f8cacc079682e879dbd
Arina <arinevercc@gmail.com>
2024-03-30 05:58:07 +0300

    Update Task 1_5

--- Task1_5/Form1.cs
+++ Task1_5/Form1.cs
@@ -24,39 +24,31 @@ namespace Task1_5
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                // Получаем путь к выбранному файлу
+
                 string filePath = openFileDialog1.FileName;
 
-                // Читаем все строки из файла
                 string[] lines = File.ReadAllLines(filePath);
 
-                // Создаем регулярное выражение для поиска группы из шести цифр
                 Regex regex = new Regex(@"\d{6}");
 
                 string directoryPath = Path.GetDirectoryName(filePath);
-
-                // Создаем новый файл для записи результатов в той же директории
                 string resultFilePath = Path.Combine(directoryPath, "result_" + Path.GetFileName(filePath));
+
                 using (StreamWriter writer = new StreamWriter(resultFilePath))
                 {
-                    // Проходим по каждой строке и заменяем все символы перед группой из шести цифр
+
                     foreach (string line in lines)
                     {
-                        // Ищем все совпадения в строке с помощью регулярного выражения
                         MatchCollection matches = regex.Matches(line);
 
-                        // Если найдены совпадения
                         if (matches.Count > 0)
                         {
-                            // Получаем индекс последнего совпадения
                             int lastIndex = matches[matches.Count - 1].Index;
 
-                            // Записываем в файл часть строки, начиная с последней найденной группы из шести цифр
                             writer.WriteLine(line.Substring(lastIndex));
                         }
                         else
                         {
-                            // Если совпадений не найдено, записываем строку как есть
                             writer.WriteLine(line);
                         }
                     }
